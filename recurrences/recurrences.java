30b8711d951a6795661a5d15deefa73e242e4d42
Doğu İlmak <doguilmak@gmail.com>
2022-07-22 18:31:54 +0300

    Update recurrences.java

--- recurrences/recurrences.java
+++ recurrences/recurrences.java
@@ -2,16 +2,16 @@ public class Programme {
 
     public static void main(String[] args) {
     
-		int tam = 1000, n;
-		float[] an = new float[tam];
-		an[0] = 0;
-		an[1] = 0;
-		an[2] = 0;
-		
-		for (n = 3; n < tam; n++) {
-	      an[n] = (((n - 3) * an[n - 1]) / n) + 1;
-	      System.out.println(an[n]);
-	    }
+	int tam = 1000, n;
+	float[] an = new float[tam];
+	an[0] = 0;
+	an[1] = 0;
+	an[2] = 0;
+
+	for (n = 3; n < tam; n++) {
+	   an[n] = (((n - 3) * an[n - 1]) / n) + 1;
+	   System.out.println(an[n]);
+	}
 		
     }
 
