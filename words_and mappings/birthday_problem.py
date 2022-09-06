2fb37f268c1228d7a72ea337148e423e84874535
Doğu İlmak <doguilmak@gmail.com>
2022-09-06 14:17:37 +0300

    Add files via upload

---
+++ words_and mappings/birthday_problem.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep  6 13:45:37 2022
+
+@author: doguilmak
+"""
+
+from math import sqrt, pi
+
+M = 365
+result = sqrt(pi * M / 2)
+
+print(f"Result of the problem: {result}")
